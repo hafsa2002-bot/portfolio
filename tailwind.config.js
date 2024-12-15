b020dfbd204bef34b6dc9daf325dc13ff5056771
Barhoud Hafsa <barhoudhafsa@gmail.com>
2024-12-15 23:32:34 +0100

    first version

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+// module.exports = {
+//     theme: {
+//       extend: {
+//         colors: {
+//           'noir': '#18181b',
+//         },
+//       }
+//     }
+//   }
+
+  export default {
+    theme: {
+      extend: {
+        colors: {
+          'my-black': '#18181b',
+        },
+      },
+    },
+  }
+
+  module.exports = {
+    theme: {
+      extend: {
+        transitionProperty: {
+          'height': 'height',
+          'spacing': 'margin',
+        }
+      }
+    }
+  }
